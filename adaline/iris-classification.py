410ce891d71c4daf0989b231b349f2ab36d38c99
Zack Moore <Zack.Moore@hlramerica.com>
2018-09-04 14:01:54 -0600

    finish implementation of adaline algorithm on iris data

--- adaline/iris-classification.py
+++ adaline/iris-classification.py
@@ -15,31 +15,46 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 # extract sepal length and petal length
 X = df.iloc[0:99, [0,2]].values
 
-# plot data
-plt.scatter(X[:49,0], X[:49,1],
-            color='red', marker='o', label='setosa')
-plt.scatter(X[49:99,0], X[49:99,1],
-            color='blue', marker='x', label='versicolor')
-plt.xlabel('sepal length [cm]')
-plt.xlabel('petal length [cm]')
-plt.legend(loc='upper left')
 
+# plot convergence of different learning rates
+# 0.01 is too large and diverges
+# 0.0001 is converging slowly
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1),
+  np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning Rate 0.01')
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1),
+  ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-squared-error)') 
+ax[1].set_title('Adaline - Learning Rate 0.0001')
+plt.show()
+
+# standardize features
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+# fit adaline with standardized features
+# convergence is now successful with a learning rate of 0.01
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
 
-ppn = Perceptron(eta=0.1, n_iter=10)
-
-ppn.fit(X,y)
-
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
 plt.xlabel('Epochs')
-plt.ylabel('Number of updates')
+plt.ylabel('Sum-squared-error')
 
 plt.tight_layout()
-plt.show()
-
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('sepal length [cm]')
-plt.xlabel('petal length [cm]')
-plt.legend(loc='upper left')
 plt.show()
