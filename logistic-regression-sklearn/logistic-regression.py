433ce68041bb2faecda81113b36e6e237f9a2cc2
Zack Moore <Zack.Moore@hlramerica.com>
2018-09-06 13:08:11 -0600

    implement logistic regression with scikit

---
+++ logistic-regression-sklearn/logistic-regression.py
@@ -0,0 +1,50 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+from graph import plot_decision_regions
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# laod iris dataset and select pedal length and width columns
+iris = datasets.load_iris()
+X = iris.data[:, [2,3]]
+y = iris.target
+
+# split data into 30% training and 70% testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# create scalar from the training data to standardize data, mean and std_dev are estimating from X_train
+# it is important to use the same scalar on training and testing data
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# train the LogisticRegression
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+
+# make predictions
+y_pred = lr.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+# graph decision regions
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=lr, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+# plt.savefig('./figures/iris_perceptron_scikit.png', dpi=300)
+plt.show()
+
+print(lr.predict_proba(X_test_std[0,:].reshape(1, -1)))
